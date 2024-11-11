3e1a5bae63c445eaa3dfba1584609d7e9fbd0558
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2024-11-11 10:34:44 +0100

    added MUI + router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import TaskListPage from './pages/TaskListPage';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TaskListPage />
+      <Router>
+        <Routes>
+          <Route path="/" Component={TaskListPage} />
+        </Routes>
+      </Router>
     </div>
   );
 }
